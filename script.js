6bc14891c1e0305e6829523086b4a027880c6343
ankit3212rajput <ANKIT2917SINGH@GMAIL.COM>
2026-04-13 14:27:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -511,7 +511,7 @@ syncAllData();
 startAutoRefresh();
 async function loadTopCoins() {
 
-const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
 const response = await fetch(url);
 const data = await response.json();
